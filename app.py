618ad01baa73295bee960587887ce94e3ba3a073
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-07 08:09:26 -0400

    adding resources and api routes

--- app.py
+++ app.py
@@ -9,7 +9,13 @@ from db import db
 from flask_migrate import Migrate
 
 # Resources
-# imports here
+from resources.rol import Rol, RolList
+from resources.employee import Employee, EmployeeList
+from resources.store import Store, StoreList
+from resources.product import Product, ProductList
+from resources.client import Client, ClientList
+from resources.invoice import Invoice, InvoiceList
+from resources.rate import Rate, RateList
 
 app = Flask(__name__)
 
@@ -28,10 +34,29 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 # Routes
-# here
+api.add_resource(Rol, '/api/v2/rol/<string:name>')
+api.add_resource(RolList, '/api/v2/roles')
+
+api.add_resource(Store, '/api/v2/store/<string:name>')
+api.add_resource(StoreList, '/api/v2/stores')
+
+api.add_resource(Employee, '/api/v2/employee/<string:name>')
+api.add_resource(EmployeeList, '/api/v2/employees')
+
+api.add_resource(Product, '/api/v2/product/<string:name>')
+api.add_resource(ProductList, '/api/v2/products')
+
+api.add_resource(Client, '/api/v2/client/<string:name>')
+api.add_resource(ClientList, '/api/v2/client')
+
+api.add_resource(Rate, '/api/v2/rate/<string:name>')
+api.add_resource(RateList, '/api/v2/rates')
+
+api.add_resource(Invoice, '/api/v2/invoice/<string:name>')
+api.add_resource(InvoiceList, '/api/v2/invoices')
 
 if __name__ == '__main__':
     app.run(
         port=os.environ.get('PORT'), 
-        debug=os.environ.get('DEBUG')
+        debug=True
     )
