f84ee22b9afc87c66a2225074674f4b68c77abd6
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-06 20:23:22 -0400

    enabling cors feature

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from datetime import timedelta
 
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from db import db
@@ -20,10 +21,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_KEY')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 
-db.init_app(app)
-migrate = Migrate(app, db)
+CORS(app)
 api = Api(app)
 jwt = JWTManager(app)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 # Routes
 # here
